bf60c27b8523516809230a4a9f49b0be742e19e7
Eduardo Marques <eduardomarqs1@gmail.com>
2021-11-14 13:52:24 -0300

    chore: set up docker

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Install composer packages
+composer dump-autoload -o --classmap-authoritative
+composer install --no-progress --ignore-platform-reqs
+composer dump-autoload -o --classmap-authoritative
+composer clear-cache
+
+# Create database
+bin/console doctrine:database:create --if-not-exists
+
+# Wait until DB is up and ready for connections
+until bin/console doctrine:query:sql "select 1" >/dev/null 2>&1; do
+  (echo >&2 "Waiting for MySQL to be ready...")
+  sleep 1
+done
+
+# Run migrations
+bin/console doctrine:migrations:migrate --no-interaction
+
+# Run FPM
+exec php-fpm
